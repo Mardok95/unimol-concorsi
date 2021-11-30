efb8689149fcbb2a3aff009c4b25b20862247084
Andrea <adimartino95@gmail.com>
2021-11-30 11:59:24 +0100

    PUNTO DI PARTENZA

--- logout.php
+++ logout.php
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 <?php
 session_start();
 session_destroy();
 // Redirect to the login page:
 header('Location: index.html');
-=======
-<?php
-session_start();
-session_destroy();
-// Redirect to the login page:
-header('Location: index.html');
->>>>>>> 0354d7dc15094fbb7c2d6706c98f57bf1e8bf1d5
 ?>
