6a65a78f76f5a7690edff499850428a49a2f7733
Andrea Di Martino <43413304+Mardok95@users.noreply.github.com>
2022-09-01 10:47:43 +0200

    Update import_data.php

--- admin/import_data.php
+++ admin/import_data.php
@@ -8,10 +8,10 @@ if (!isset($_SESSION['loggedin'])) {
 }
 
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'unimol_concorsi';
+$DATABASE_HOST = '++++';
+$DATABASE_USER = '++++';
+$DATABASE_PASS = '++++';
+$DATABASE_NAME = '++++';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
