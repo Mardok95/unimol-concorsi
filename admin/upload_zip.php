4706b1fd0210db53325f07f78e57fe088b54b16a
Andrea Di Martino <43413304+Mardok95@users.noreply.github.com>
2022-09-01 10:49:12 +0200

    Update upload_zip.php

--- admin/upload_zip.php
+++ admin/upload_zip.php
@@ -14,10 +14,10 @@ require 'config.php';
 <?php 
 
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'unimol_concorsi';
+$DATABASE_HOST = '++++';
+$DATABASE_USER = '++++';
+$DATABASE_PASS = '++++';
+$DATABASE_NAME = '++++';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
